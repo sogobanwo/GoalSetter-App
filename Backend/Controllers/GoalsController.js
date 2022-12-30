e412cebc271de8e76cf2ba7842b72061065267c4
sogobanwo <sogobanwo@gmail.com>
2022-12-30 16:31:50 +0100

    Goals controller, errorHandler and middleware SetUo

--- Backend/Controllers/GoalsController.js
+++ Backend/Controllers/GoalsController.js
@@ -1,43 +1,46 @@
+const asyncHandler = require("express-async-handler")
+
+
 // @desc     get all goals
 // @Route    GET /api/goals
 // @access   Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
   res.status(200).json({
     Message: "Get All Goals",
   });
-};
+});
 
 // @desc     Set a goal
 // @Route    POST /api/goals
 // @access   Private
-const setGoal = (req, res) => {
-  if(!req.body.text){
-    res.status(400).json({
-      message: "Please input a text"
-    })
+const setGoal =asyncHandler(async (req, res) => {
+  if(!req.body.goal){
+    res.status(400)
+    throw new Error("Please add a text field")
   }
   res.status(200).json({
     Message: "Set a Goal",
   });
-};
+});
 
 // @desc     Update a goal
+
 // @Route    PUT /api/goals/:id
 // @access   Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler (async(req, res) => {
   res.status(200).json({
     Message: `Update Goal ${req.params.id}`,
   });
-};
+});
 
 // @desc     Delete a goal
 // @Route    DELETE /api/goals/:id
 // @access   Private
-const deleteGoal = (req, res) => {
+const deleteGoal =asyncHandler (async(req, res) => {
   res.status(200).json({
     Message: `delete Goal ${req.params.id}`,
   });
-};
+});
 
 module.exports = {
   getGoals,
--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const {errorHandler} = require("./middleware/errorMiddleware")
 const port = process.env.PORT
 
 const app = express()
@@ -10,6 +11,9 @@ app.use(express.urlencoded({ extended: false }))
 
 // base route
 app.use("/api/goals", require("./Routes/GoalRoutes"))
+
+// errorHandler
+app.use(errorHandler)
 
 app.listen(port, ()=> console.log(`Your app is running on port ${port}`))
 
