fb3b4ba5edaa12bd40209400db99bb533d49a501
sogobanwo <sogobanwo@gmail.com>
2022-12-30 22:38:31 +0100

    RestApi for the Goal App

--- Backend/Controllers/GoalsController.js
+++ Backend/Controllers/GoalsController.js
@@ -32,18 +32,31 @@ const setGoal =asyncHandler(async (req, res) => {
 // @Route    PUT /api/goals/:id
 // @access   Private
 const updateGoal = asyncHandler (async(req, res) => {
-  res.status(200).json({
-    Message: `Update Goal ${req.params.id}`,
-  });
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal){
+    res.status(400)
+    throw new Error("Goal not found")
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc     Delete a goal
 // @Route    DELETE /api/goals/:id
 // @access   Private
 const deleteGoal =asyncHandler (async(req, res) => {
-  res.status(200).json({
-    Message: `delete Goal ${req.params.id}`,
-  });
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal){
+    res.status(400)
+    throw new Error("Goal not found")
+  }
+
+   await goal.remove()
+  res.status(200).json({id: req.params.id});
 }); 
 
 module.exports = {
